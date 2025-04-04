61bf967cbabd5346dfb35c091c618292da026c47
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-04-04 23:58:08 +0300

    step 3 add form with formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,31 @@
+import { Formik, Form, Field } from "formik";
 import ContactList from "../ContactList/ContactList";
+import { useId } from "react";
+
+const initialValues = {
+  username: "",
+  email: "",
+};
 
 const ContactForm = () => {
-  return null;
-  //   return (
-  //     <ul>
-  //       {contacts.map(({ id, name, number }) => (
-  //         <li key={id}>
-  //           <ContactList name={name} number={number} id={id}></ContactList>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   );
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form>
+        <label htmlFor={nameFieldId}>Name</label>
+        <Field type="text" name="username" id={nameFieldId} />
+        <label htmlFor={numberFieldId}>Number</label>
+        <Field type="number" name="number" id={numberFieldId} />
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
