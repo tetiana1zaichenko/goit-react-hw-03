40bbd9fa2a4b27b1fd84805214ec46eb5ccb5b2a
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-04-05 00:32:10 +0300

    step 3 add validation with yup

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,6 +2,19 @@ import { Formik, Form, Field } from "formik";
 import ContactList from "../ContactList/ContactList";
 import { useId } from "react";
 import { nanoid } from "nanoid";
+import * as Yup from "yup";
+import { ErrorMessage } from "formik";
+
+const FeedbackSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  number: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
 
 const initialValues = {
   name: "",
@@ -21,12 +34,22 @@ const ContactForm = () => {
     actions.resetForm();
   };
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
       <Form>
-        <label htmlFor={nameFieldId}>Name</label>
-        <Field type="text" name="name" id={nameFieldId} />
-        <label htmlFor={numberFieldId}>Number</label>
-        <Field type="number" name="number" id={numberFieldId} />
+        <div>
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field type="text" name="name" id={nameFieldId} />
+          <ErrorMessage name="name" component="span" />
+        </div>
+        <div>
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field type="number" name="number" id={numberFieldId} />
+          <ErrorMessage name="number" component="span" />
+        </div>
         <button type="submit">Add contact</button>
       </Form>
     </Formik>
